91a23ccb25d0e155fca0a8af9263984f7fe04537
Lovmar28 <fathinmughits2@upi.edu>
2025-10-22 08:13:49 +0700

    ganti vite1321312e1wdvwjvk

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  cacheDir: "/tmp/.vite", // Pindah ke /tmp
   preview: {
     port: parseInt(process.env.PORT) || 8080,
     host: true,
