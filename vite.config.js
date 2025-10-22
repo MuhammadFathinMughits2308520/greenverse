ee35ce3244de35a8df4c89d7d2de944e5ecac7a3
Lovmar28 <fathinmughits2@upi.edu>
2025-10-22 07:52:15 +0700

    remove railway.json for Railway auto build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  cacheDir: "/tmp/vite-cache",
+  cacheDir: process.env.VITE_CACHE_DIR || ".vite",
   preview: {
     port: parseInt(process.env.PORT) || 8080,
     host: true,
